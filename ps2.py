1cfd0a3948712c2c3501547f0468e02f7f8fc1e2
krumpr <75940729+vivekagarwal2349@users.noreply.github.com>
2021-09-08 12:41:25 +0530

    Update ps2.py

--- ps2.py
+++ ps2.py
@@ -1,6 +1,6 @@
 from Box2D.Box2D import b2PolygonShape
 from Box2D.examples.framework import (Framework, Keys, main)
-from Box2D import (b2CircleShape, b2EdgeShape, b2FixtureDef, b2PolygonShape,
+from Box2D import (b2World, b2CircleShape, b2EdgeShape, b2FixtureDef, b2PolygonShape,
                    b2Transform, b2Mul,
                    b2_pi)
 from Box2D import *
@@ -196,57 +196,39 @@ class ps2Arena (Framework):
             shapes=b2EdgeShape(vertices=[(-7*6*0.0254, -2*6*0.0254 ), (-8*6*0.0254, -2*6*0.0254)])
         )
         
-        self.world.gravity = (0.0, 0.0)
-        # ground0 = self.world.CreateStaticBody(
-        #     position=(0,7*6*0.0254),
-        #     shapes=b2PolygonShape(box=(14*6*0.0254, 0.0254/4))
-        # )
-        # ground1 = self.world.CreateStaticBody(
-        #     position=(0,-7*6*0.0254),
-        #     shapes=b2PolygonShape(box=(14*6*0.0254, 0.0254/4))
-        # )
-        # ground2 = self.world.CreateStaticBody(
-        #     position=(7*6*0.0254,0),
-        #     shapes=b2PolygonShape(box=( 0.0254/4, 14*6*0.0254))
-        # )
-        # ground3 = self.world.CreateStaticBody(
-        #     position=(-7*6*0.0254, 0),
-        #     shapes=b2PolygonShape(box=( 0.0254/4,14*6*0.0254))
-        # )
-
-        # radius = 1.0
-        # density = 1.0
-        # # The bodies
-        # for i, restitution in enumerate([0.0, 0.1, 0.3, 0.5, 0.75, 0.9, 1.0]):
-        #     self.world.CreateDynamicBody(
-        #         position=(-10 + 3.0 * i, 20),
-        #         fixtures=b2FixtureDef(
-        #             shape=b2CircleShape(radius=radius),
-        #             density=density, restitution=restitution)
-        #     )
-
-
+        # self.world=self.world(gravity=(0,0))
         radius = 2*0.0254
-        density = 0.1
+        density = 1.0
         restitution = 0.0
-        self.body = self.world.CreateDynamicBody(position=(-6*6*0.0254, 0), fixtures=b2FixtureDef(shape=b2CircleShape(radius=radius), density=density, restitution=restitution))
-
+        self.body = self.world.CreateDynamicBody(position=(7.5*6*0.0254, 1.5*6*0.0254), fixtures=b2FixtureDef(shape=b2CircleShape(radius=radius), density=density, restitution=restitution))
+        
+        self.world.gravity=(0,0)
+        # damping parameter
+        self.body.linearDamping = 0.9
 
     def Keyboard(self, key):
+        
         if not self.body:
             return
+
         if key == Keys.K_w:
-            f = self.body.GetWorldVector(localVector=(0.0, -0.005))
-            p = self.body.GetWorldPoint(localPoint=(0.0, 0.005))
+            f = self.body.GetWorldVector(localVector=(0.0, 0.005))
+            p = self.body.GetWorldPoint(localPoint=(0.0, 0.00))
             self.body.ApplyForce(f, p, True)
         elif key == Keys.K_a:
-            self.body.ApplyTorque(0.0001, True)
+            f = self.body.GetWorldVector(localVector=(-0.005, 0.0))
+            p = self.body.GetWorldPoint(localPoint=(0.0, 0.00))
+            self.body.ApplyForce(f, p, True)
         elif key == Keys.K_d:
-            self.body.ApplyTorque(-0.0001, True)
+            f = self.body.GetWorldVector(localVector=(0.005, 0.0))
+            p = self.body.GetWorldPoint(localPoint=(0.0, 0.00))
+            self.body.ApplyForce(f, p, True)
         elif key == Keys.K_s:
             f = self.body.GetWorldVector(localVector=(0.0, -0.005))
-            p = self.body.GetWorldPoint(localPoint=(0.0, -0.005))
+            p = self.body.GetWorldPoint(localPoint=(0.0, 0.00))
             self.body.ApplyForce(f, p, True)
+
+        
 
 if __name__ == "__main__":
     main(ps2Arena)
